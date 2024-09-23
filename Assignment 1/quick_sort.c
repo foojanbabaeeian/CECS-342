cf51adf3c6a7b5382a4a3746c4a80974e581cf75
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-09-22 22:10:13 -0700

    checking the output
    check

--- Assignment 1/quick_sort.c
+++ Assignment 1/quick_sort.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-// Helper function to swap two elements
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
 // Partition function for quick sort
-int partition(int *a, int low, int high) {
-    int pivot = a[high];  // Choosing the last element as the pivot
+int partition(int *a, int low, int high)
+{
+    int pivot = a[high]; // Choosing the last element as the pivot
     int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (a[j] <= pivot) {
+    for (int j = low; j < high; j++)
+    {
+        if (a[j] <= pivot)
+        {
             i++;
             swap(&a[i], &a[j]);
         }
@@ -22,15 +25,17 @@ int partition(int *a, int low, int high) {
 }
 
 // Quick sort function
-void quick_sort_recursive(int *a, int low, int high) {
-    if (low < high) {
+void quick_sort_recursive(int *a, int low, int high)
+{
+    if (low < high)
+    {
         int pi = partition(a, low, high);
         quick_sort_recursive(a, low, pi - 1);  // Recursively sort the left part
         quick_sort_recursive(a, pi + 1, high); // Recursively sort the right part
     }
 }
 
-void quick_sort(int *a, int n) {
+void quick_sort(int *a, int n)
+{
     quick_sort_recursive(a, 0, n - 1);
 }
-
