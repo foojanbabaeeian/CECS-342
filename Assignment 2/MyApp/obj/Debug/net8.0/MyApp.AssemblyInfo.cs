53014ce816c4cd09d8c0bacaaadd7e0129e74715
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-20 23:25:15 -0700

    IDK what i am pushing
    I really have no idea. I am going through a lot

--- Assignment 2/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ Assignment 2/MyApp/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ccb5f5654db716034cd0afea4ce533cb617753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7a5843aea1023ca957977fa169d04540588a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
